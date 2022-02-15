b088ee62c17aaa3fecf1b3047fc1c37aef152354
itaytzdaka <itaytzdaka1@gmail.com>
2022-02-15 15:30:40 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const server = express();
+
+const cors = require("cors");
+
+server.use(cors({
+    origin: "http://localhost:3001",
+    credentials: true
+}));
+
+
+const forecastController=require("./controllers/forecast-controller");
+
+// server.use("/api/weather", weatherController);
+server.use("/api/forecast", forecastController);
+
+
+server.use(express.json());
+
+const path = require("path");
+server.use(express.static(path.join(__dirname, "./_front-end")));
+server.use("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "./_front-end/index.html"));
+});
+
+
+const port=process.env.PORT || 3000;
+server.listen(port, ()=>console.log("Listening on port " + port + " http://localhost:3000"));
