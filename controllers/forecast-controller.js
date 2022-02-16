2399066eb0cae11da9939680685d457cb959b28a
itaytzdaka <itaytzdaka1@gmail.com>
2022-02-16 13:04:05 +0200

    responsive improve and forecast for hours

--- controllers/forecast-controller.js
+++ controllers/forecast-controller.js
@@ -6,7 +6,7 @@ const axios = require("axios");
 router.get("/:city", async (request, response) => {
     try {
         const city = request.params.city;
-        const forecast = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=f1b31511aeac4bc4944132424221302&q=${city}&aqi=no`);
+        const forecast = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=f1b31511aeac4bc4944132424221302&q=${city}&aqi=no&days=2`);
         const data = forecast.data;
         response.json(data);
     }
